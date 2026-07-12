ad762e3b8b3f648de9ccc0038bd46f89235288dc
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-07-12 18:14:01 +0200

    added basic functionality

---
+++ script.js
@@ -0,0 +1,53 @@
+var tokenButton = document.getElementById('tokenButton');
+var tokenDisplay = document.getElementById('tokenDisplay');
+
+var upgradeClicker = document.getElementById('upgradeClicker');
+var autoClicker = document.getElementById('autoClicker');
+
+var duskTokens = 0;
+
+var multiplier = 1;
+var multiplierCost = 25;
+
+var autoClickers = 0;
+var autoClickerCost = 15;
+
+function displayTokenAmount() {
+    tokenDisplay.innerText = "Dusk Tokens: " + Math.round(duskTokens * 100) / 100;
+}
+
+tokenButton.addEventListener('click', function() {
+    duskTokens += multiplier;
+    displayTokenAmount();
+});
+
+upgradeClicker.addEventListener('click', function() {
+    if (duskTokens >= multiplierCost) {
+        duskTokens -= multiplierCost;
+        multiplierCost *= 1.3;
+        multiplier ++;
+        displayTokenAmount();
+        upgradeClicker.innerText = "Upgrade Clicker (Cost: " + Math.round(multiplierCost * 100) / 100 + ")";
+    } else {
+        alert("Not enough Dusk Tokens!");
+    }
+});
+
+autoClicker.addEventListener('click', function() {
+    if (duskTokens >= autoClickerCost) {
+        duskTokens -= autoClickerCost;
+        autoClickers ++;
+        autoClickerCost *= 1.25;
+        displayTokenAmount();
+        autoClicker.innerText = "Buy Auto Clicker (Cost: " + Math.round(autoClickerCost * 100) / 100 + ")";
+    } else {
+        alert("Not enough Dusk Tokens!");
+    }
+});
+
+setInterval(function() {
+    if (autoClickers <= 0) { return; }
+
+    duskTokens += autoClickers * 0.1;
+    displayTokenAmount();
+}, 1000/6);
