2c719324a9bbd73e843d4cfb3f2f34f1c40d007c
Christina A. H. <47138990+CafCat49@users.noreply.github.com>
2026-07-14 02:06:01 +0200

    preparing for version 2.0
    whole lot of features incoming

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var tpsDisplay = document.getElementById('tpsDisplay');
 
 var upgradeClicker = document.getElementById('upgradeClicker');
 var autoClicker = document.getElementById('autoClicker');
+var hireRetainer = document.getElementById('hireRetainer');
+var upgradeRetainer = document.getElementById('upgradeRetainer');
+var resetGame = document.getElementById('resetGame');
 
 var duskTokens = 0;
 
@@ -13,12 +16,16 @@ var multiplierCost = 25;
 var autoClickers = 0;
 var autoClickerCost = 15;
 
+var retainers = 0;
+var retainerCost = 100;
+
 function displayTokenAmount() {
     tokenDisplay.innerText = "Dusk Tokens: " + Math.round(duskTokens);
 }
 
 function displayTPS() {
-    tpsDisplay.innerText = "Tokens per second: " + Math.round(autoClickers);
+    var tps = (autoClickers * multiplier) + (retainers * 10);
+    tpsDisplay.innerText = "Tokens per second: " + Math.round(tps);
 }
 
 tokenButton.addEventListener('click', function() {
@@ -51,9 +58,50 @@ autoClicker.addEventListener('click', function() {
     }
 });
 
-setInterval(function() {
-    if (autoClickers <= 0) { return; }
+hireRetainer.addEventListener('click', function() {
+    if (duskTokens >= retainerCost) {
+        duskTokens -= retainerCost;
+        retainers += 1;
+        retainerCost *= 1.2;
+        displayTokenAmount();
+        displayTPS();
+        hireRetainer.innerText = "Hire Retainer (Cost: " + Math.round(retainerCost) + ")";
+    } else {
+        alert("Not enough Dusk Tokens!");
+    }
+});
 
+upgradeRetainer.addEventListener('click', function() {});
+
+// Tick functions
+setInterval(function() {
     duskTokens += autoClickers;
+    duskTokens += retainers * 10;
     displayTokenAmount();
 }, 1000);
+
+setInterval(function() {
+    if (duskTokens >= multiplierCost) {
+        upgradeClicker.classList.remove('inactive');
+    }
+    else
+    {
+        upgradeClicker.classList.add('inactive');
+    }
+
+    if (duskTokens >= autoClickerCost) {
+        autoClicker.classList.remove('inactive');
+    }
+    else
+    {
+        autoClicker.classList.add('inactive');
+    }
+
+    if (duskTokens >= retainerCost) {
+        hireRetainer.classList.remove('inactive');
+    }
+    else
+    {
+        hireRetainer.classList.add('inactive');
+    }
+}, 10);
